925903263b1ec9d3db7f1bd0cdeb5d3e527fe035
vatsva <87061470+vatsva@users.noreply.github.com>
2021-07-07 12:48:15 +0530

    Day 1 assignment submitted

---
+++ Day 1 .js
@@ -0,0 +1,17 @@
+console.log("\n Print 5 '*' Horizontally")
+
+for (i=0; i<1; i++)
+{
+    c = ""
+    for (j=0; j<5; j++)
+    {
+        c=c + "* "
+    }
+    console.log(c);
+}
+
+console.log("\n Print 5 '*' Vertically")
+for(i=0; i<5; i++)
+{
+  console.log("* ")
+}
